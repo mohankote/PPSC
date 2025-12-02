ea29b3a80e299225e250710308119e76bd2bdbd2
Kote Mohan <manojkote90@gmail.com>
2025-12-02 09:39:44 +0530

    Add files via upload

---
+++ using string to find vowel, constants, digits,symbols.cpp
@@ -0,0 +1,25 @@
+
+
+#include<stdio.h>
+int main() {
+	char a[30];
+	int i,v=0,d=0,s=0,c=0;
+	scanf("%[^\n]s", a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+		v++;
+		else if (a[i]==' ')
+		s++;
+		else if (a[i]>='0' && a[i]<='9')
+		d++;
+		else 
+		c++;
+	}
+	printf("Vowels are :%d\nDigits are :%d\nSymbols are :%d\nConstants are: %d" ,v,d,s,c);
+	
+	return 0;
+	
+	
+	
+}
